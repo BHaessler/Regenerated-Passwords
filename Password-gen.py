7e2ed678430e47aff6b365b5786a0408550b38d9
eahaessler <41296427+eahaessler@users.noreply.github.com>
2023-06-15 11:57:01 -0400

    Update Password-gen.py
    Updated for bugs, readability and organization.

--- Password-gen.py
+++ Password-gen.py
@@ -1,30 +1,29 @@
-# this is a password generator that will ask the user how many passwords they 
-# want generated, how difficult they want the passwords to be and how long 
+# this is a password generator that will ask the user how many passwords they
+# want generated, how difficult they want the passwords to be and how long
 # they want them to be
 
-#calling modules 
+#calling modules
+"""random module for picking letters for the passwords"""
 import random
 
-#defining global variables
-userNumber = 0
-userLen = 0
-userDiffi = "0"
 
-
-#asks the user for their input 
+#---------------------------------------------------------------------------------
 def get_user_input():
-    userNumber = input("How many passwords would you like to create today?\n")
+    """gathers user inputs"""
+
+    user_number = input("How many passwords would you like to create today?\n")
     print("Thanks")
-    userLen = input("How long would you like your passwords to be?\n")
+    user_len = input("How long would you like your passwords to be?\n")
     print("Thanks again. Final Question")
-    userDiffi = input("""How difficult would you like your passwords to be (e =
+    user_diffi = input("""How difficult would you like your passwords to be (e =
      just letters, uppercase and lowercase, m = letters and numbers, h = letters, 
      numbers and symbols) [enter corresponding letter]?\n""").lower()
-    return userNumber, userDiffi, userLen
+    return user_number, user_diffi, user_len
 
 
-#a function that creates the charactor list to randomly pick from 
 def create_char_list(difficulty):
+    """creates a list of viable characters/letters for the passwords to be made from"""
+
     e_list = ["a","A","b","B","c","C","d","D","e","E","f","F","g","G","h","H",
             "i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q"
             ,"Q","r","R","s","S","t","T","v","V","w","W","x","X","y","Y","z","Z"]
@@ -33,19 +32,22 @@ def create_char_list(difficulty):
     final_char_list = []
 
     if difficulty == "e":
-        final_char_list = final_char_list + e_list 
-        return (final_char_list)
+        final_char_list = final_char_list + e_list
+        return final_char_list
 
-    elif difficulty == "m": 
+    elif difficulty == "m":
         final_char_list = e_list + m_list_adds
-        return(final_char_list)
+        return final_char_list
 
     elif difficulty == "h":
         final_char_list = e_list + m_list_adds + h_list_adds
-        return(final_char_list)
+        return final_char_list
 
-#a function that creates as many passwords as the user has asked for 
+
 def create_passwd(char_list, number_of_pws, pwd_len):
+    """Given the characters to pick from, the number of passwords to make, and the
+    length, this will create the passwords the user has asked for"""
+
     password_list = []
 
     for password in range(0, number_of_pws):
@@ -54,25 +56,37 @@ def create_passwd(char_list, number_of_pws, pwd_len):
             password_string = password_string + random.choice(char_list)
         password_list.append(password_string)
 
-    return(password_list)
+    return password_list
 
-#a function that prints the final password list
+
 def final_printout(password_list):
+    """a function that prints the final password list"""
     print("\nThese are your randomly generated passwords:")
     for password in password_list:
         print(password)
     print("\n")
     return None
 
-#main code is here 
-#calls functions in order of usage
-userNumber, userDiffi, userLen = get_user_input()
-userNumber = int(userNumber)
-userLen = int(userLen)
-# print(userNumber)
-# print(userDiffi)
-# print(userLen)
-char_list = create_char_list(userDiffi)
-#print(char_list)
-password_final = create_passwd(char_list, userNumber, userLen)
-final_printout(password_final)
+
+def main_func():
+    """main code is here, calls functions in order of usage"""
+
+    #defining global variables
+    user_number = 0
+    user_len = 0
+    user_diffi = "0"
+
+    #taking in user input
+    user_number, user_diffi, user_len = get_user_input()
+    user_number = int(user_number)
+    user_len = int(user_len)
+    # print(user_number)
+    # print(user_diffi)
+    # print(user_len)
+    char_list = create_char_list(user_diffi)
+    #print(char_list)
+    password_final = create_passwd(char_list, user_number, user_len)
+    final_printout(password_final)
+    return
+
+main_func()
